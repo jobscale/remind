972a0e74e27c6116c33a86954b87067fa6404fe1
jobscale <jobscale@users.noreply.github.com>
2023-10-24 07:23:24 +0900

    remind list updated

--- app/list.js
+++ app/list.js
@@ -1,32 +1,7 @@
 const list = [{
-  schedule: '2023-10-05 13:45+0900',
-  title: 'kepco.jp',
-  text: 'ビンゴカード',
-  url: 'https://kepco.jp/miruden/mypage/hapiepoint/hapitabingo',
-}, {
-  schedule: '2023-10-05 12:00+0900',
+  schedule: '2023-10-24 08:00+0900',
   title: 'Discord',
   text: 'Discord POTATO',
-}, {
-  schedule: '2023-10-06 13:45+0900',
-  title: 'kepco.jp',
-  text: 'ビンゴカード',
-  url: 'https://kepco.jp/miruden/mypage/hapiepoint/hapitabingo',
-}, {
-  schedule: '2023-10-07 13:45+0900',
-  title: 'kepco.jp',
-  text: 'ビンゴカード',
-  url: 'https://kepco.jp/miruden/mypage/hapiepoint/hapitabingo',
-}, {
-  schedule: '2023-10-08 13:45+0900',
-  title: 'kepco.jp',
-  text: 'ビンゴカード',
-  url: 'https://kepco.jp/miruden/mypage/hapiepoint/hapitabingo',
-}, {
-  schedule: '2023-10-09 13:45+0900',
-  title: 'kepco.jp',
-  text: 'ビンゴカード',
-  url: 'https://kepco.jp/miruden/mypage/hapiepoint/hapitabingo',
 }, {
   schedule: '2023-10-25 13:45+0900',
   title: 'kepco.jp',
@@ -37,6 +12,14 @@ const list = [{
   title: 'kepco.jp',
   text: 'ビンゴカード',
   url: 'https://kepco.jp/miruden/mypage/hapiepoint/hapitabingo',
+}, {
+  schedule: '2023-11-11 18:00+0900',
+  title: 'Beer Session Event',
+  text: 'Beer Session Event\nBeer Bar 🍺 06-6947-3530',
+}, {
+  schedule: '2023-11-12 08:00+0900',
+  title: 'Beer Session Event',
+  text: 'Beer Session Event\nBeer Bar 🍺 06-6947-3530',
 }];
 
 module.exports = {
